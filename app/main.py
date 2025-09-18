cf5be436351800862b3cb8d63149cf1cafa32295
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-18 12:47:33 +0200

    Critical blockers fix

--- app/main.py
+++ app/main.py
@@ -37,12 +37,12 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def serve_cars(self, cars: list[Car]) -> float:
-        income = 0
+        income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        return round(income / 1)
+        return round(income , 1)
 
     def rate_service(self, new_rating: int | float) -> None:
         new_average = (
