55272eec933fd523c630b192151efaf84f07b078
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-18 11:14:09 +0200

    add rate_service method , fixing bugs in calculate_washing_price

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         wash_price = round(car.comfort_class * (
-                    self.count_of_ratings - car.clean_mark) * self.average_rating / self.distance_from_city_center, 1)
+                    self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center, 1)
         return wash_price
 
     def wash_single_car(self, car: Car) -> None:
@@ -32,8 +32,10 @@ class CarWashStation:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
-                car.clean_mark = self.clean_power
+                self.wash_single_car(car)
         return income
 
-    def rate_service(self, car: Car) -> None:
-        pass
+    def rate_service(self, new_rating: int) -> None:
+        new_average = (self.average_rating * self.count_of_ratings + new_rating) / (self.count_of_ratings + 1)
+        self.average_rating = round(new_average,1)
+        self.count_of_ratings += 1
